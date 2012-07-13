ce965e34e7ef1d347a32c810a92e5dd12696a96f
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-07-13 13:28:55 +0200

    Added unique index on gi on protein_gi_to_taxids

--- db/sql/create_tables/protein_gi_to_taxids.sql
+++ db/sql/create_tables/protein_gi_to_taxids.sql
@@ -1,4 +1,8 @@
-CREATE TABLE protein_gi_to_taxids AS (
+CREATE TABLE protein_gi_to_taxids (
   gi	INTEGER,
   taxid	INTEGER
 );
+
+CREATE UNIQUE INDEX protein_gi_to_taxids_gi_uindex ON
+  protein_gi_to_taxids(gi)
+;
