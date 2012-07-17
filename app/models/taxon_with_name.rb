453c64c2785d1d771a02f3d544623ad74cefda6f
Johannes Alneberg <alneberg@kth.se>
2012-07-17 10:40:59 +0200

    Recursive all_up_to_root method for taxon_with_name and children typo fix

--- app/models/taxon_with_name.rb
+++ app/models/taxon_with_name.rb
@@ -20,7 +20,22 @@ class TaxonWithName < ActiveRecord::Base
   self.primary_key = 'taxon_id'
 
   belongs_to :parent, :class_name => 'TaxonWithName', :foreign_key => :parent_taxon_id
-  belongs_to :children, :class_name => 'TaxonWithName', :foreign_key => :parent_taxon_id
+  has_many :children, :class_name => 'TaxonWithName', :foreign_key => :parent_taxon_id
 
   has_many :protein_gi_taxons, :foreign_key => :taxon_id
+  
+  def all_up_to_root
+    all_up_to_root_rec(self, [])
+  end
+
+  private
+  
+  def all_up_to_root_rec(twn, ancestors)
+    ancestors << twn
+    if twn.parent and twn.parent != twn
+      all_up_to_root_rec(twn.parent, ancestors)
+    else
+      return ancestors
+    end
+  end
 end
