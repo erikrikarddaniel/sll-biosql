dcd187a586bdbbbdd2e50583ba5c60f02d4e735e
Johannes Alneberg <alneberg@kth.se>
2012-07-17 10:42:42 +0200

    all_taxa_with_ancestors and association to taxon_with_name for sequenced_genome

--- app/models/sequenced_genome.rb
+++ app/models/sequenced_genome.rb
@@ -8,4 +8,13 @@
 
 class SequencedGenome < ActiveRecord::Base
   attr_accessible :ncbi_taxon_id, :sequenced
+  has_one :taxon_with_name, :foreign_key => :ncbi_taxon_id
+
+  def self.all_taxa_with_ancestors
+    taxa = []
+    self.all.each do |gold_genome|
+      taxa << gold_genome.taxon_with_name.all_up_to_root
+    end
+    return taxa
+  end
 end
