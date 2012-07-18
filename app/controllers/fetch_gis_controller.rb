69f40e3a2fc44d7dfc2042c1c51cb590195a2bd4
Johannes Alneberg <alneberg@kth.se>
2012-07-18 12:58:02 +0200

    Debugging with view for fetch_gis

--- app/controllers/fetch_gis_controller.rb
+++ app/controllers/fetch_gis_controller.rb
@@ -2,7 +2,12 @@ class FetchGisController < ApplicationController
   # GET /fetch_gis
   # GET /fetch_gis.json
   def index
-    @fetch_gis = ProteinGiTaxon.find_all_by_protein_gi(params["gis"], :include => :taxon_with_name)
+    if params["gis"]
+      gis = params["gis"]
+    else
+      gis = [95109515, 95109515, 107836860]
+    end
+    @fetch_gis = ProteinGiTaxon.find_all_by_protein_gi(gis, :include => :taxon_with_name)
     
     @fetch_gis.each do |pgt|
       pgt.ncbi_taxon_id = pgt.taxon_with_name.ncbi_taxon_id
