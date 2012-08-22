4c8870257ab6c272844476ed10db9dba6982d860
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-08-22 18:07:21 +0200

    Added sequenced_genomes test data. Moved test data file so that they start with numbers to fix insert order.

---
+++ non_rails_stuff/testdata/100.sequenced_genomes.sql
@@ -0,0 +1,15 @@
+INSERT INTO sequenced_genomes(ncbi_taxon_id, wgs)
+  VALUES(544404, true)
+;
+INSERT INTO sequenced_genomes(ncbi_taxon_id, wgs)
+  VALUES(83334, true)
+;
+INSERT INTO sequenced_genomes(ncbi_taxon_id, wgs)
+  VALUES(208964, true)
+;
+INSERT INTO sequenced_genomes(ncbi_taxon_id, wgs)
+  VALUES(9606, true)
+;
+INSERT INTO sequenced_genomes(ncbi_taxon_id, wgs)
+  VALUES(63221, true)
+;
