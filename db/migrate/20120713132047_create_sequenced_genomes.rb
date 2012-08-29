eae4107c8238ee8b7149829591ebe96d8597606e
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-08-29 09:19:54 +0200

    Deleting migration to create sequence_genomes table -- can't get creation of primary key ncbi_taxon_id to work, asks for a table ncbi_taxons. Will move this to a non_rails_stuff/ddl sql script (next commit).

--- db/migrate/20120713132047_create_sequenced_genomes.rb
+++
@@ -1,8 +0,0 @@
-class CreateSequencedGenomes < ActiveRecord::Migration
-  def change
-    create_table :sequenced_genomes, :primary_key => 'ncbi_taxon_id' do |t|
-      t.integer :ncbi_taxon_id, :null => false
-      t.boolean :wgs
-    end
-  end
-end
