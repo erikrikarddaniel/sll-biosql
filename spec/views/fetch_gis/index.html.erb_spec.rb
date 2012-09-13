de2e8abde31ee6fa14dc6b751b41cedac18abc42
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-09-13 13:23:45 +0200

    View tests work

--- spec/views/fetch_gis/index.html.erb_spec.rb
+++ spec/views/fetch_gis/index.html.erb_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
 describe "fetch_gis/index" do
-#  before(:each) do
-#    assign(:fetch_gis, [
-#      stub_model(FetchGi,
-#        :gi => 1
-#      ),
-#      stub_model(FetchGi,
-#        :gi => 1
-#      )
-#    ])
-#  end
-#
-#  it "renders a list of fetch_gis" do
-#    render
-#    # Run the generator again with the --webrat flag if you want to use webrat matchers
-#    assert_select "tr>td", :text => 1.to_s, :count => 2
-#  end
+  before(:each) do
+    assign(:fetch_gis, [
+      stub_model(FetchGi,
+        :gi => 1
+      ),
+      stub_model(FetchGi,
+        :gi => 10
+      )
+    ])
+  end
+
+  it "renders a list of fetch_gis" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => 1.to_s, :count => 1
+    assert_select "tr>td", :text => 10.to_s, :count => 1
+    assert_select "tr>td", :count => 2
+  end
 end
