918980177492fab73b622744827ce0ad551ad420
Daniel Lundin <daniel.lundin@scilifelab.se>
2012-12-05 13:05:13 +0100

    Improvements (?) to migrate.sh and populate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-bundle exec rake db:migrate && bundle exec annotate --position=before && bundle exec rake db:populate 
+bundle exec rake db:migrate && bundle exec annotate --position=before
 
 # Here we first setup an empty test database, then populate it with some non
 # Rails data that some tests are dependent on.
