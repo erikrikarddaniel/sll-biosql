cb37eae6ca6d1abdbf4e7c63abac3a6d428071b0
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-09-07 00:20:30 +0200

    Tests for getting a list of ncbi taxon ids and returning the hierarchy for each

--- spec/controllers/taxon_with_names_controller_spec.rb
+++ spec/controllers/taxon_with_names_controller_spec.rb
@@ -21,6 +21,23 @@ describe TaxonWithNamesController do
     end
   end
 
+  describe "GET ncbi_taxon_ids2full_taxon_hierarchies" do
+    it "returns a list of json representations of taxons full hierarchies" do
+      get :ncbi_taxon_ids2full_taxon_hierarchies, { ncbi_taxon_id: [83334,63221], format: 'json' }, valid_session
+      response.should be_success
+      entries = JSON.parse(response.body)
+      entries.should be_a(Array)
+      entries.length.should == 2
+      entries[0][0]['scientific_name'].should == 'Escherichia coli O157:H7'
+      entries[0][1]['scientific_name'].should == 'Escherichia coli'
+      entries[0][2]['scientific_name'].should == 'Escherichia'
+      entries[0][3]['scientific_name'].should == 'Enterobacteriaceae'
+      entries[1][0]['scientific_name'].should == 'Homo sapiens neanderthalensis'
+      entries[2][1]['scientific_name'].should == 'Homo sapiens'
+      entries[3][2]['scientific_name'].should == 'Homo'
+      entries[4][3]['scientific_name'].should == 'Homininae'
+    end
+  end
   describe "GET ncbi_taxon_id2full_taxon_hierarchy from name" do
     it "returns a json representation of a taxons full hierarchy" do
       get :ncbi_taxon_id2full_taxon_hierarchy, { name: 'Escherichia coli O157:H7', format: 'json' }, valid_session
