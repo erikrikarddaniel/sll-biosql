c7c681503eae5ddb71f6169799ff05f81397ccc2
Brynjar Smari Bjarnason <binni@binnisb.com>
2013-11-26 18:06:31 +0100

    Python script for parsing gi_queue and fetch protein sequences and metadata from NCBI

---
+++ script/fetch_seqs_from_gis.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+from Bio import Entrez
+from Bio import SeqIO
+from BioSQL import BioSeqDatabase
+import psycopg2
+import logging
+
+# <codecell>
+
+# CONSTANTS
+LOG_FILE = "/tmp/fetch_seqs_from_gis"
+EMAIL = "binni@binnisb.com"
+TOOL = "sll_biosql"
+DB_NAME = "sll_biosql"
+
+# BASE LOGGING CONFIG
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    filemode='a', # append to log file
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s'
+)
+
+# <codecell>
+
+# GET bioentries gis that have already been imported.
+# GET gi_queues that have not been imported
+# SELECT thouse gis that are not fetched and not in bioentry
+with psycopg2.connect("dbname='biosqldev' user='binni' host='localhost' password=''") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""SELECT biodatabase_id FROM biodatabase WHERE name = %s""",(DB_NAME,))
+        biodb_id = cur.fetchone()
+        cur.execute("""SELECT identifier FROM bioentry WHERE biodatabase_id = %s""", biodb_id)
+        bioentry_ids = set(map(lambda x: x[0], cur.fetchall()))
+        logging.info("found {0} gis in bioentry".format(len(bioentry_ids)))
+        cur.execute("""SELECT gi FROM gi_queues WHERE fetched IS false""")
+        gi_queues_ids = set(map(lambda x: x[0], cur.fetchall()))
+        logging.info("found {0} gis in qi_queues".format(len(gi_queues_ids)))
+
+        fetch_gis = gi_queues_ids - bioentry_ids
+        logging.info("will fetch {0} gis from ncbi ({1})".format(len(fetch_gis),",".join(fetch_gis)))
+
+# <codecell>
+
+# SELECT the biodatabase to use
+# GET proteins from NCBI
+# IMPORT proteins to bioentries
+# LOG which gis imported and which failed
+try:
+    server = BioSeqDatabase.open_database(driver="psycopg2", user="binni", 
+                                      passwd="", host="localhost", 
+                                      db="biosqldev")
+    if DB_NAME in server:
+        db = server[DB_NAME]
+    else:
+        db = server.new_database(DB_NAME, description="sll biosql test")
+        server.commit()
+        
+    Entrez.email = EMAIL
+    Entrez.tool = TOOL
+    
+    fh = Entrez.efetch(db="protein", rettype="gb", retmode="text",id=list(fetch_gis))
+    seqs = list(SeqIO.parse(fh, "genbank"))
+    logging.info("Fetched {0} sequences from NCBI.".format(len(seqs)))
+    count = db.load(seqs)
+    logging.info("Inserted {0} sequences into biosql".format(count))
+    
+    if count != len(fetch_gis):
+        logging.warn("Not equally many gis to fetch as were inserted")
+    not_fetched_gis = set()
+    for i in list(fetch_gis):
+        try:
+            entry = db.lookup(gi=i)
+            logging.info("Gi: {0} inserted in database".format(i))
+        except IndexError as e:
+            logging.warn("Gi: {0} not inserted in database".format(i))
+            not_fetched_gis.add(i)
+    server.commit()
+
+finally:
+    fh.close()
+    server.close()
+
+# <codecell>
+
+# UPDATE gi_queue entries that have been fetched
+with psycopg2.connect("dbname='biosqldev' user='binni' host='localhost' password=''") as conn:
+    with conn.cursor() as cur:
+        
+        cur.execute("""UPDATE gi_queues SET fetched=true,updated_at=now() WHERE gi IN ('{0}')"""
+                    .format("','".join(fetch_gis - not_fetched_gis)))
+        logging.info("Updated status of {0} gi_queues rows out of {1} gis that were added to bioentry".format(cur.rowcount,count))
+
